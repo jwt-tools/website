d1778313f21c97a30e2338ac3f537266b82b9762
mfmurray <mfmurray@umich.edu>
2023-11-06 17:20:41 -0500

    fix: react import

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Header.scss';
+import classnames from 'classnames';
 
 const Header: React.FC = () => {
+  const { pathname } = useLocation();
   return (
     <header>
       <img src="" alt="logo" />
       <div className="header-links">
-        <NavLink to={'/'}>JWT Decoded</NavLink>
-        <NavLink to={'/history'}>History</NavLink>
-        <NavLink to={'/jwt'}>What are JWTs?</NavLink>
+        <NavLink
+          className={classnames('header-link', {
+            'header-link-active': pathname === '/',
+          })}
+          to={'/'}
+        >
+          JWT Decoded
+        </NavLink>
+        <NavLink className={'header-link'} to={'/history'}>
+          History
+        </NavLink>
+        <NavLink className={'header-link'} to={'/jwt'}>
+          What are JWTs?
+        </NavLink>
       </div>
+      <div>Built by Rownd</div>
     </header>
   );
 };
