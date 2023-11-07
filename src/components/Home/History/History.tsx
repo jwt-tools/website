df1b0f685b254f43977a70e7ccfa18c2f33b2a92
mfmurray <mfmurray@umich.edu>
2023-11-07 15:40:16 -0500

    feat: history item skeleton

--- src/components/Home/History/History.tsx
+++ src/components/Home/History/History.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './History.scss';
+import Trash from '../../../assets/trash-can.svg';
+import Edit from '../../../assets/edit.svg';
 
 const History: React.FC = () => {
+  const items = [{ time: '2023-11-07T20:00:07Z' }];
   return (
     <div className="history">
       <h1>History</h1>
+      {items.map(({ time }) => {
+        const date = new Date(time);
+        return (
+          <div key={`history-${time}`} className="history__item">
+            <div className="history__item__date">
+              {date.toString()}
+              <img src={Edit} />
+            </div>
+            <div className="history__item__buttons">
+              <button className="text-button">View</button>
+              <img src={Trash} />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
