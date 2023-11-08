dc271acff4840dbe691342fe0f125221a83184f9
mfmurray <mfmurray@umich.edu>
2023-11-08 13:37:57 -0500

    feat: added more content to footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Rownd from '../../assets/Rownd-white.svg';
+import LinkedIn from '../../assets/logo--linkedin.svg';
 import './Footer.scss';
 import { NavLink } from 'react-router-dom';
 
@@ -8,13 +9,35 @@ const Footer: React.FC = () => {
     <div className="footer">
       <div className="footer__title">JWT.IS is brought to you by</div>
       <NavLink target="_blank" to="https://rownd.io/">
-        <img src={Rownd} alt="Rownd" />
+        <img className="footer__img__big" src={Rownd} alt="Rownd" />
       </NavLink>
       <div className="footer__subtitle">
         Securely implement frictionless authentication with JWTs using Rownd on
         any stack and any device in less than 10 minutes.
       </div>
-      <button>Get started for free</button>
+      <a target="_blank" href="https://rownd.io/" className="secondary-button">
+        Get started for free
+      </a>
+
+      <div className="footer__bottom">
+        <div className="footer__bottom__built">
+          Built by{' '}
+          <NavLink to="https://rownd.io/">
+            <img src={Rownd} />
+          </NavLink>
+        </div>
+        <div className="footer__bottom_right">
+          <a target="_blank" className="link" href="https://docs.rownd.io">
+            Rownd docs
+          </a>
+          <a
+            href="https://www.linkedin.com/company/rownd/"
+            className="footer__follow-us"
+          >
+            Follow us on <img src={LinkedIn} />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
