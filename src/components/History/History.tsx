31fe24f9f6fe9d116e39cc802eaaf5b73c621db4
mfmurray <mfmurray@umich.edu>
2023-11-09 13:15:27 -0500

    feat: history show more button

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './History.scss';
 import Trash from '../../assets/trash-can.svg';
 import Edit from '../../assets/edit.svg';
 import { getAllTokens, Token, deleteToken, getToken } from '../../storage/db';
+import classNames from 'classnames';
+
+const MAX_LENGTH = 4;
 
 const History: React.FC<{
   setToken: (e: string) => void;
   tokens: Token[];
   setTokens: (e: Token[]) => void;
 }> = ({ setToken, setTokens, tokens }) => {
+  const [showAll, setShowAll] = useState(false);
 
   React.useEffect(() => {
     (async () => {
@@ -43,11 +47,17 @@ const History: React.FC<{
       {
         tokens.length === 0 && <div className='history__empty-state'>No history to show</div>
       }
-      {tokens.map((token) => {
+      {tokens.slice(0, showAll ? tokens.length : MAX_LENGTH).map((token, idx) => {
         const date = new Date(token.created);
         const time = date.toISOString();
+        const isFirst = idx === 0;
+        const isLast = tokens.length === (idx + 1);
+
         return (
-          <div key={`history-${time}`} className="history__item">
+          <div key={`history-${time}`} className={classNames("history__item", {
+            "history__item--first": isFirst,
+            "history__item--last": isLast && !showAll
+          })}>
             <div className="history__item__date">
               {date.toString()}
               <img src={Edit} alt="edit" />
@@ -71,6 +81,9 @@ const History: React.FC<{
           </div>
         );
       })}
+
+      {tokens.length > MAX_LENGTH && <div className='history__show-more'>
+        <button onClick={() => setShowAll((prev) => !prev)} className='text-button'>{showAll ? 'Show less' : 'Show more'}</button></div>}
     </div>
   );
 };
