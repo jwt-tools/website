1051458f225c2fe96581ca6f7589a7d86595f2d2
Matt Hamann <mhamann@rownd.io>
2023-11-09 13:09:09 -0500

    chore: enable pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'JSON Web Token (JWT) Debugger',
+        short_name: 'JWTDebugger',
+        description: 'This JWT debugging tool is made to help developers quickly understand the header, claims, and signature of any JWT.',
+        theme_color: '#262626',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     sourcemap: true,
   },
