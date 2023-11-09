89ebd138eb96b4fe2caef25ec7155e2160620aa0
Matt Hamann <mhamann@rownd.io>
2023-11-09 14:04:29 -0500

    feat(pwa): set maskable icon

--- vite.config.ts
+++ vite.config.ts
@@ -17,12 +17,14 @@ export default defineConfig({
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any'
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
