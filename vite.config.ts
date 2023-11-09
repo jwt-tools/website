48f7ff28a4b079ce6d5ea6ec2eedcc06c606444e
Matt Hamann <mhamann@rownd.io>
2023-11-09 14:17:49 -0500

    fix(pwa): maskable icon

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,13 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-603x603-mask.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
